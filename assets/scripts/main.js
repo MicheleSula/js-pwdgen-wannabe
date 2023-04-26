37a60cd6bcf723375c13a532d06c737eb9090dfd
Sula Michele <sulamichele95@gmail.com>
2023-04-26 17:30:52 +0200

    Added error message

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,24 +7,37 @@ const min = 1;
 const max = 99;
 
 // Name
-userName = prompt ("Qual'è il tuo nome?");
+userName = prompt ("Qual è il tuo nome?");
 
 // Surname
-userSurname = prompt ("Qual'è il tuo cognome?");
+userSurname = prompt ("Qual è il tuo cognome?");
 
 // Colour
-userColour = prompt ("Qual'è il tuo colore preferito?");
+userColour = prompt ("Qual è il tuo colore preferito?");
 
 // RandomNumber
 randomNumber = getRandomNumber(min, max);
 
-// Password
-userPassword = userName + userSurname + userColour + randomNumber;
-document.getElementById("password").innerHTML = userPassword;
+// Password output
+userPassword = passwordFunction (userName, userSurname, userColour);
+
+
+// Functions
 
 // RNG function
 function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Input checker
+function passwordFunction (userName, userSurname, userColour) {
+    if (userName == 0 || userSurname == 0 || userColour == 0) {
+        document.getElementById("password").innerHTML = "Non hai compilato tutti i campi!";
+        document.getElementById("password").style.color = 'Red';
+        document.getElementById("password-container").style.border = '2px red solid';
+    } else {
+        document.getElementById("password").innerHTML = userName + userSurname + userColour + randomNumber;
+    }
 }
